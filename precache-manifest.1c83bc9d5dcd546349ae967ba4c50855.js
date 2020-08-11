545f75e163210c9c4291f645e72efe8e21594633
ZhannaL <zhannet1814@gmail.com>
2020-08-11 14:50:14 +0200

    Deploy f2d44f8-dirty

---
+++ precache-manifest.1c83bc9d5dcd546349ae967ba4c50855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50194ce3ba2d4594bf1805793f6a73ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c66327cdd0ccd1d15d20",
+    "url": "/static/css/main.c7dd276b.chunk.css"
+  },
+  {
+    "revision": "4bf13116fcecd46fb601",
+    "url": "/static/js/2.c068f87b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c068f87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66327cdd0ccd1d15d20",
+    "url": "/static/js/main.086eef48.chunk.js"
+  },
+  {
+    "revision": "c3c192b93606727ae2f7",
+    "url": "/static/js/runtime-main.cd3bc300.js"
+  },
+  {
+    "revision": "0dcb9d2b6e6efcd0edc6d30eb7032faa",
+    "url": "/static/media/quiz-unknown.0dcb9d2b.png"
+  }
+]);
--- src/Components/Finish/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Finish } from './Finish';
